989ede6103d1c67a69c0ffdaf53ad01b3089c787
Susurrus <104753102+Susurrus0@users.noreply.github.com>
2025-07-08 20:22:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 mkdir -p ~/.local/share/applications/
 
-mkdir -p ~/.sustools
-cd ~/.sustools
+mkdir -p ~/.susurrus0
+cd ~/.susurrus0
 wget -O file.zip # link to release
 unzip -o file.zip
 rm file.zip
