efdfada32535bf12c8724890ac1e7c917accdda3
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2025-07-09 22:23:46 +0200

    Create initial ui elements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'designernZJzvj.ui'
+## Form generated from reading UI file 'testWvBreO.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.1
 ##
@@ -11,28 +11,90 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QGridLayout, QHBoxLayout,
+    QLabel, QMainWindow, QMenu, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QVBoxLayout,
+    QWidget)
+import sys
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(724, 419)
+            MainWindow.setObjectName(u"Gothic Deck Tools")
+        MainWindow.resize(516, 335)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.horizontalLayout.addWidget(self.pushButton_2)
+
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.checkBox_2 = QCheckBox(self.centralwidget)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+
+        self.verticalLayout.addWidget(self.checkBox_2)
+
+        self.checkBox_4 = QCheckBox(self.centralwidget)
+        self.checkBox_4.setObjectName(u"checkBox_4")
+
+        self.verticalLayout.addWidget(self.checkBox_4)
+
+        self.checkBox_3 = QCheckBox(self.centralwidget)
+        self.checkBox_3.setObjectName(u"checkBox_3")
+
+        self.verticalLayout.addWidget(self.checkBox_3)
+
+        self.checkBox = QCheckBox(self.centralwidget)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.verticalLayout.addWidget(self.checkBox)
+
+
+        self.gridLayout.addLayout(self.verticalLayout, 2, 0, 1, 1)
+
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.gridLayout.addWidget(self.pushButton_3, 3, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 724, 24))
+        self.menubar.setGeometry(QRect(0, 0, 516, 24))
+        self.menuTesting = QMenu(self.menubar)
+        self.menuTesting.setObjectName(u"menuTesting")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuTesting.menuAction())
+        self.menuTesting.addSeparator()
+        self.menuTesting.addSeparator()
 
         self.retranslateUi(MainWindow)
 
@@ -41,5 +103,24 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Gothic 1", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Gothic 2", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Choose config options:", None))
+        self.checkBox_2.setText(QCoreApplication.translate("MainWindow", u"Fix1", None))
+        self.checkBox_4.setText(QCoreApplication.translate("MainWindow", u"Fix2", None))
+        self.checkBox_3.setText(QCoreApplication.translate("MainWindow", u"Tweak1", None))
+        self.checkBox.setText(QCoreApplication.translate("MainWindow", u"Tweak2", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Run", None))
+        self.menuTesting.setTitle(QCoreApplication.translate("MainWindow", u"Testing", None))
     # retranslateUi
 
+def main():
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
